2817c0de93ed46e83b2069a7b5f3acad2c4ddafb
steveh7 <stevehoudart7@gmail.com>
2026-06-30 00:07:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-====================
+// =====================================================
 // TrailLab v1.1
 // Partie 1 - Initialisation
 // =====================================================
@@ -126,8 +126,7 @@ function analyseParcours(points){
     let elevations = [];
 
     let tempsTotal = 0;
-
-    // Parcours de tous les points GPX
+        // Parcours de tous les points GPX
     for(let i=0;i<points.length;i++){
 
         const p = points[i];
@@ -241,7 +240,6 @@ function analyseParcours(points){
     drawChart(distances,elevations);
 
 }
-
 // =====================================================
 // Résumé automatique TrailLab
 // =====================================================
@@ -356,7 +354,6 @@ function generateSummary(data){
     document.getElementById("summaryText").textContent = texte;
 
 }
-
 // =====================================================
 // Graphique altimétrique
 // =====================================================
